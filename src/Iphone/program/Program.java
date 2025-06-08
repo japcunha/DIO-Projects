3da5252b279845239af473d39ff4ea670b21df36
Joane <138986247+japcunha@users.noreply.github.com>
2025-06-08 10:11:41 -0300

    first project

---
+++ src/Iphone/program/Program.java
@@ -0,0 +1,19 @@
+package Iphone.program;
+
+public class Program {
+    public static void main(String[] args) {
+        Iphone meuIphone = new Iphone();
+
+        meuIphone.selectMusic("Imagine - John Lennon");
+        meuIphone.play();
+        meuIphone.pause();
+
+        meuIphone.connect("1234-4567");
+        meuIphone.attend();
+        meuIphone.startVoiceMail();
+
+        meuIphone.showPage("https://www.showdaspoderosas.com.br");
+        meuIphone.addBrowserTab();
+        meuIphone.upDatePage();
+    }
+}
